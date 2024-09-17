23d2b9f9c26f7fcdfab4bdefd06d154a23f4ccb6
Mat Swatek <azcards32@gmail.com>
2024-09-16 18:30:21 -0700

    Update NoLockout24Playoffs.py

--- NoLockout24Playoffs.py
+++ NoLockout24Playoffs.py
@@ -280,8 +280,6 @@ df_finals['OBP'] = df_finals['OnBase']/df_finals['PA']
 
 
 df_week24 = all_weeks[all_weeks['Week']==24]
-
-
 cols = ['Team','AB','Innings']
 df_week24 = df_week24[cols]
 df_week24.set_index('Team')
